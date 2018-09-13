d0a5742de79d600cf236ed444db4fde520a1bfb6
kushaldeveloper <43170976+kushaldeveloper@users.noreply.github.com>
2018-09-13 16:33:26 +0530

    add

---
+++ www/js/myFunctions.js
@@ -0,0 +1,42 @@
+		//window.addEventListener("batterystatus", onBatteryStatus, false);
+		//window.addEventListener("batterylow", onBatteryLow, false);
+		//window.addEventListener("batterycritical", onBatteryCritical, false);
+		document.addEventListener("deviceready", onDeviceReady,false);
+		document.addEventListener("cameraTakePicture",cameraTakePicture,false);
+		//("click", cameraTakePicture); 
+		function onDeviceReady() {
+		alert("Device is starting");
+		cameraTakePicture();
+		//batteryStatus();
+		}
+		function cameraTakePicture() { 
+					alert('camera is starting');
+					alert('camera:' + navigator.camera);
+					alert('camera:' + navigator.camera.getPicture);
+					navigator.camera.getPicture(onSuccess, onFail, {  
+					quality: 50, 
+					destinationType: Camera.DestinationType.DATA_URL 
+					   });  
+					   
+					   function onSuccess(imageData) { 
+					   alert('sucess');
+						  var image = document.getElementById('myImage'); 
+						  image.src = "data:image/jpeg;base64," + imageData; 
+					   }  
+					   
+					   function onFail(message) { 
+						  alert('Failed because: ' + message); 
+					   } 
+					
+					}
+		/*function batteryStatus(){
+		function onBatteryStatus(info) { 
+		   alert("BATTERY STATUS:  Level: " + info.level + " isPlugged: " + info.isPlugged); 
+		}
+		function onBatteryLow(info) {
+    		alert("Battery Level Low " + info.level + "%");
+		}
+		function onBatteryCritical(info) {
+    		alert("Battery Level Critical " + info.level + "%\nRecharge Soon!");
+		}
+	}*/
