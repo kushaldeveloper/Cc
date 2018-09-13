8b202dd741ce93b3757ba5b28fe893fcff95ead7
kushaldeveloper <43170976+kushaldeveloper@users.noreply.github.com>
2018-09-13 16:34:56 +0530

    ch

--- www/js/myFunctions.js
+++ www/js/myFunctions.js
@@ -2,7 +2,8 @@
 		//window.addEventListener("batterylow", onBatteryLow, false);
 		//window.addEventListener("batterycritical", onBatteryCritical, false);
 		document.addEventListener("deviceready", onDeviceReady,false);
-		document.addEventListener("cameraTakePicture",cameraTakePicture,false);
+		document.getElementById("cameraTakePicture").addEventListener 
+		("click", cameraTakePicture); 
 		//("click", cameraTakePicture); 
 		function onDeviceReady() {
 		alert("Device is starting");
