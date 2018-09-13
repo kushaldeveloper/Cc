1fe86129b5c46aafec5b3a01bd97674aa7bd88c5
kushaldeveloper <43170976+kushaldeveloper@users.noreply.github.com>
2018-09-13 13:38:46 +0530

    add app

---
+++ www/js/myFunction.js
@@ -0,0 +1,34 @@
+		//window.addEventListener("batterystatus", onBatteryStatus, false);
+		//window.addEventListener("batterylow", onBatteryLow, false);
+		//window.addEventListener("batterycritical", onBatteryCritical, false);
+		document.addEventListener("deviceready", onDeviceReady,false);
+		function onDeviceReady() {
+		alert("Device is starting");
+		cameraTakePicture();
+		//batteryStatus();
+		}
+		function cameraTakePicture() { 
+		alert('camera is starting');
+			navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+				destinationType: Camera.DestinationType.FILE_URI });
+
+			function onSuccess(imageURI) {
+				var image = document.getElementById('myImage');
+				image.src = imageURI;
+			}
+
+			function onFail(message) {
+				alert('Failed because: ' + message);
+			}
+		}
+		/*function batteryStatus(){
+		function onBatteryStatus(info) { 
+		   alert("BATTERY STATUS:  Level: " + info.level + " isPlugged: " + info.isPlugged); 
+		}
+		function onBatteryLow(info) {
+    		alert("Battery Level Low " + info.level + "%");
+		}
+		function onBatteryCritical(info) {
+    		alert("Battery Level Critical " + info.level + "%\nRecharge Soon!");
+		}
+	}*/
