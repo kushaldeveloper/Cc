11427881b8f55dfcbce01a0405babe6d487205b7
kushaldeveloper <43170976+kushaldeveloper@users.noreply.github.com>
2018-09-13 15:34:24 +0530

    ch

--- www/js/myFunction.js
+++ www/js/myFunction.js
@@ -11,20 +11,20 @@
 		}
 		function cameraTakePicture() { 
 		alert('camera is starting');
-		//alert('camera:' + navigator.camera);
-		//alert('camera:' + navigator.camera.getPicture);
+		alert('camera:' + navigator.camera);
+		alert('camera:' + navigator.camera.getPicture);
 			navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
 				destinationType: Camera.DestinationType.FILE_URI });
 
-			function onSuccess(imageURI) {
+			function onSuccess(imageData) {
 				var image = document.getElementById('myImage');
-				image.src = imageURI;
+				image.src = "data:image/jpeg;base64," + imageData;
 			}
 
 			function onFail(message) {
 				alert('Failed because: ' + message);
-			}
 		}
+	}
 		/*function batteryStatus(){
 		function onBatteryStatus(info) { 
 		   alert("BATTERY STATUS:  Level: " + info.level + " isPlugged: " + info.isPlugged); 
